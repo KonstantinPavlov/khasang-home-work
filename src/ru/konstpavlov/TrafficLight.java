4ddffd4c714713d7c6209dc0d7ff160927b04eed
KonstantinPavlov <konstpavlovsdr@gmail.com>
2016-09-29 21:49:54 +0300

    Add implementing to Light interface

--- src/ru/konstpavlov/TrafficLight.java
+++ src/ru/konstpavlov/TrafficLight.java
@@ -4,33 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TrafficLight{
-    public static void main(String[] args) throws IOException {
-        double input=inputMethod();
-        System.out.println(getLight(input));
-    }
+public class TrafficLight implements Light {
 
-    private static String getLight(double input){
-        switch ((int)(input % 5))
-        {
-            case 0 : return "Красный";
-            case 1 : return "Зеленый";
-            case 2 : return "Зеленый";
-            case 3 : return "Желтый";
-            case 4 : return "Красный";
+    private String getLight(double input) {
+        switch ((int) (input % 5)) {
+            case 0:
+                return Colors.RED.toString();
+            case 1:
+                return Colors.GREEN.toString();
+            case 2:
+                return Colors.GREEN.toString();
+            case 3:
+                return Colors.YELLOW.toString();
+            case 4:
+                return Colors.RED.toString();
         }
         return null;
     }
 
-    private static double inputMethod(){
+    private double inputMethod() {
         System.out.println("Введите минуту, для определения света светофора:");
-        double time= 0;
-        try ( BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+        double time = 0;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             time = Double.parseDouble(reader.readLine());
-            while (time>60.0)
-            {
+            while (time > 60.0) {
                 System.out.println("В часе 60 минут, введите корректное время:");
-                time=Double.parseDouble(reader.readLine());
+                time = Double.parseDouble(reader.readLine());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,4 +37,8 @@ public class TrafficLight{
         return time;
     }
 
+    @Override
+    public void start() {
+        System.out.println(getLight(inputMethod()));
+    }
 }
